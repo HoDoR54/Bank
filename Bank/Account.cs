b3da03addd23303151013592c560a96b2b2f1cc7
Hpone Tauk Nyi <hponetaukyou@gmail.com>
2025-01-16 22:58:18 +0630

    Add project files.

---
+++ Bank/Account.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    internal class Account
+    {
+        public bool haveAccount = false;
+        public string AccountHolder;
+        private string Password;
+
+        public void TakePassword (string password) { 
+            if (string.IsNullOrEmpty(password))
+            {
+                Console.Clear();
+                Console.WriteLine("Please enter a valid password.");
+            } else
+            {
+                Password = password;
+            }
+        }
+    }
+}
